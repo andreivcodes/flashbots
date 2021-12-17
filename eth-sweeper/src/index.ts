beef035ac0daf16a6b03cf825345ac5c41148c8c
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2021-12-18 00:31:01 +0200

    v0.1

---
+++ eth-sweeper/src/index.ts
@@ -0,0 +1,141 @@
+import {
+  FlashbotsBundleProvider,
+  FlashbotsBundleRawTransaction,
+  FlashbotsBundleResolution,
+  FlashbotsBundleTransaction,
+} from "@flashbots/ethers-provider-bundle";
+import { BigNumber, providers, Wallet, ethers } from "ethers";
+import { checkSimulation, gasPriceToGwei, printTransactions } from "./utils";
+import { config } from "dotenv";
+config();
+
+require("log-timestamp");
+
+const BLOCKS_IN_FUTURE = 1;
+
+const GWEI = BigNumber.from(10).pow(9);
+const PRIORITY_GAS_PRICE = GWEI.mul(30);
+
+const PRIVATE_KEY_SOURCE = process.env.PRIVATE_KEY_SOURCE || "";
+const PUBLIC_KEY_DESTINATION = process.env.PUBLIC_KEY_DESTINATION || "";
+const FLASHBOTS_RELAY_SIGNING_KEY =
+  process.env.FLASHBOTS_RELAY_SIGNING_KEY || "";
+
+if (PRIVATE_KEY_SOURCE === "") {
+  console.warn(
+    "Must provide PRIVATE_KEY_SOURCE environment variable, corresponding to Ethereum EOA with assets to be transferred"
+  );
+  process.exit(1);
+}
+if (PUBLIC_KEY_DESTINATION === "") {
+  console.warn(
+    "Must provide PUBLIC_KEY_DESTINATION environment variable, an address which will receive assets"
+  );
+  process.exit(1);
+}
+if (FLASHBOTS_RELAY_SIGNING_KEY === "") {
+  console.warn(
+    "Must provide FLASHBOTS_RELAY_SIGNING_KEY environment variable. Please see https://github.com/flashbots/pm/blob/main/guides/flashbots-alpha.md"
+  );
+  process.exit(1);
+}
+
+async function ethsweeper() {
+  let simulatedGasPrice = BigNumber.from(-100);
+
+  const walletRelay = new Wallet(FLASHBOTS_RELAY_SIGNING_KEY);
+  const ETHEREUM_RPC_URL = process.env.ETHEREUM_RPC_URL;
+  const provider = new providers.StaticJsonRpcProvider(ETHEREUM_RPC_URL);
+  const flashbotsProvider = await FlashbotsBundleProvider.create(
+    provider,
+    walletRelay
+  );
+
+  const walletSource = new Wallet(PRIVATE_KEY_SOURCE);
+
+  let signedBundle: string[];
+  let bundleTransactions: (
+    | FlashbotsBundleTransaction
+    | FlashbotsBundleRawTransaction
+  )[];
+
+  while (simulatedGasPrice < BigNumber.from(0)) {
+    const block = await provider.getBlock("latest");
+
+    const gasPrice = PRIORITY_GAS_PRICE.add(block.baseFeePerGas || 0);
+
+    const currentBalance = await provider.getBalance(walletSource.address);
+
+    let value = (await provider.getBalance(walletSource.address)).sub(
+      gasPrice.mul(21000)
+    );
+
+    console.log(
+      `current balance: ${ethers.utils.formatEther(
+        currentBalance
+      )}ETH   current gas: ${gasPriceToGwei(
+        gasPrice
+      )}    current gas cost: ${gasPriceToGwei(
+        gasPrice.mul(21000)
+      )}    expected value: ${ethers.utils.formatEther(value)}ETH`
+    );
+
+    if (value > BigNumber.from(0)) {
+      bundleTransactions = [
+        {
+          transaction: {
+            to: PUBLIC_KEY_DESTINATION,
+            gasPrice: gasPrice,
+            value: value,
+            gasLimit: 21000,
+          },
+          signer: walletSource,
+        },
+      ];
+      signedBundle = await flashbotsProvider.signBundle(bundleTransactions);
+      await printTransactions(bundleTransactions, signedBundle);
+      simulatedGasPrice = await checkSimulation(
+        flashbotsProvider,
+        signedBundle
+      );
+
+      console.log("simulatedGasPrice: " + simulatedGasPrice);
+    }
+  }
+
+  provider.on("block", async (blockNumber) => {
+    const simulatedGasPrice = await checkSimulation(
+      flashbotsProvider,
+      signedBundle
+    );
+    const targetBlockNumber = blockNumber + BLOCKS_IN_FUTURE;
+    console.log(
+      `Current Block Number: ${blockNumber},   Target Block Number:${targetBlockNumber},   gasPrice: ${gasPriceToGwei(
+        simulatedGasPrice
+      )} gwei`
+    );
+    const bundleResponse = await flashbotsProvider.sendBundle(
+      bundleTransactions,
+      targetBlockNumber
+    );
+    if ("error" in bundleResponse) {
+      throw new Error(bundleResponse.error.message);
+    }
+    const bundleResolution = await bundleResponse.wait();
+    if (bundleResolution === FlashbotsBundleResolution.BundleIncluded) {
+      console.log(`Congrats, included in ${targetBlockNumber}`);
+      process.exit(0);
+    } else if (
+      bundleResolution === FlashbotsBundleResolution.BlockPassedWithoutInclusion
+    ) {
+      console.log(`Not included in ${targetBlockNumber}`);
+    } else if (
+      bundleResolution === FlashbotsBundleResolution.AccountNonceTooHigh
+    ) {
+      console.log("Nonce too high, bailing");
+      process.exit(1);
+    }
+  });
+}
+
+ethsweeper();
---
+++ eth-sweeper/src/utils.ts
@@ -0,0 +1,85 @@
+import {
+  FlashbotsBundleProvider,
+  FlashbotsBundleRawTransaction,
+  FlashbotsBundleTransaction,
+} from "@flashbots/ethers-provider-bundle";
+import { BigNumber } from "ethers";
+import { parseTransaction } from "ethers/lib/utils";
+
+export const ETHER = BigNumber.from(10).pow(18);
+export const GWEI = BigNumber.from(10).pow(9);
+
+export async function checkSimulation(
+  flashbotsProvider: FlashbotsBundleProvider,
+  signedBundle: Array<string>
+): Promise<BigNumber> {
+  const simulationResponse = await flashbotsProvider.simulate(
+    signedBundle,
+    "latest"
+  );
+
+  if ("results" in simulationResponse) {
+    for (let i = 0; i < simulationResponse.results.length; i++) {
+      const txSimulation = simulationResponse.results[i];
+      if ("error" in txSimulation) {
+        throw new Error(
+          `TX #${i} : ${txSimulation.error} ${txSimulation.revert}`
+        );
+      }
+    }
+
+    if (simulationResponse.coinbaseDiff.eq(0)) {
+      throw new Error("Does not pay coinbase");
+    }
+
+    const gasUsed = simulationResponse.results.reduce(
+      (acc: number, txSimulation) => acc + txSimulation.gasUsed,
+      0
+    );
+
+    const gasPrice = simulationResponse.coinbaseDiff.div(gasUsed);
+    return gasPrice;
+  }
+
+  return BigNumber.from(-1);
+}
+
+export async function printTransactions(
+  bundleTransactions: Array<
+    FlashbotsBundleTransaction | FlashbotsBundleRawTransaction
+  >,
+  signedBundle: Array<string>
+): Promise<void> {
+  console.log("--------------------------------");
+  console.log(
+    (
+      await Promise.all(
+        bundleTransactions.map(async (bundleTx, index) => {
+          const tx =
+            "signedTransaction" in bundleTx
+              ? parseTransaction(bundleTx.signedTransaction)
+              : bundleTx.transaction;
+          const from =
+            "signer" in bundleTx ? await bundleTx.signer.getAddress() : tx.from;
+
+          return `TX #${index}: ${from} => ${tx.to} : ${tx.data}`;
+        })
+      )
+    ).join("\n")
+  );
+
+  console.log("--------------------------------");
+  console.log(
+    (
+      await Promise.all(
+        signedBundle.map(async (signedTx, index) => `TX #${index}: ${signedTx}`)
+      )
+    ).join("\n")
+  );
+
+  console.log("--------------------------------");
+}
+
+export function gasPriceToGwei(gasPrice: BigNumber): number {
+  return gasPrice.mul(100).div(GWEI).toNumber() / 100;
+}
